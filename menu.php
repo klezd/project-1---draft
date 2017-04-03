89115ede15dad9f537d92376381fe9c1a9d7d20d
Unknown <roxydao97@gmail.com>
2017-04-03 10:00:56 +0300

    home page updated #1

---
+++ menu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Asian Aroma </title>
+        <link href="mycss.css" type="text/css"  rel="stylesheet">
+    </head>
+    <body>
+
+      <table id="login">
+        <tr>
+          <td><button type="button" name="signin">Sign in</button></td>
+          <td><button type="button" name="signup">Sign up</button></td>
+        </tr>
+      </table>
+
+      <div id = "menu">
+        <ul>
+          <li><a href="home.php">Home</a></li>
+          <li><a href="home.php">Home</a></li>
+          <li><a href="home.php">Home</a></li>
+        </ul>
+      </div>
+
+      <div id="content">
+        
