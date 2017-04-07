5a52d359885a63cc70ed3f2e933b9f4df9c9823d
Unknown <roxydao97@gmail.com>
2017-04-07 10:58:13 +0300

    signin and signup form edited

--- signin.php
+++ signin.php
@@ -1,20 +1,22 @@
-<?php include "menu.php"; ?>
+<?php include "navigator.php"; ?>
 
 <h2 style="text-align:center;"> Welcome back, USER! </h2>
-<form class="form-horizontal signinform" method="post" action="" style="width:500px; padding: 10px; margin:auto;">
-  <div class="form-group">
+<form  method="post" action="" style="width:500px; padding: 10px; margin:auto;">
+  <fieldset>
+  <legend> Sign in here </legend>
+  <div class="form-group row">
     <label class="control-label col-sm-2" for="uname">Username</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="username" placeholder="Enter username" required>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-2" for="pwd">Password</label>
     <div class="col-sm-10">
       <input type="password" class="form-control" id="pwd" placeholder="Enter password" required>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <div class="col-sm-offset-2 col-sm-6">
 
         <div>
@@ -32,5 +34,6 @@
       <button type="button" class="btn btn-default" name="btnCancel">Cancel</button>
     </div>
   </div>
+</fieldset>
 </form>
 <?php include "footer.php"; ?>
--- signup.php
+++ signup.php
@@ -1,31 +1,31 @@
-<?php include "menu.php"; ?>
+<?php include "navigator.php"; ?>
 <h2 style="text-align:center;">Sign Up</h2>
-<form class="form-horizontal signupform" method="post" action="" style="width:500px; padding: 10px; margin:auto;">
-  <div class="form-group">
+<form method="post" action="" style="width:500px; padding: 10px; margin:auto;">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="name">Name</label>
     <div class="col-sm-8">
       <input type="text" class="form-control" id="name" placeholder="Enter your name" required>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="email">Email </label>
     <div class="col-sm-8">
       <input type="email" class="form-control" id="email" placeholder="Enter email" required>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="uname">Username</label>
     <div class="col-sm-8">
       <input type="text" class="form-control" id="uname" placeholder="Enter username" required>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="pwd">Password</label>
     <div class="col-sm-8">
       <input type="password" class="form-control" id="pwd" placeholder="Enter password" required>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="rePwd">Repeat Password</label>
     <div class="col-sm-8">
       <input type="password" class="form-control" id="rePwd" placeholder="Enter password again" required>
@@ -40,22 +40,23 @@
       <div id="validPass"></div>
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="tele">Telephone Number </label>
     <div class="col-sm-8">
       <input type="tel" class="form-control" id="tele" placeholder="Enter telephone number">
     </div>
   </div>
-  <div class="form-group">
+  <div class="form-group row">
     <label class="control-label col-sm-4" for="add">Address</label>
     <div class="col-sm-8">
       <input type="text" class="form-control" id="add" placeholder="Enter address">
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-6">
+    <div class="col-sm-offset-7 col-sm-8">
+       &emsp;
       <button type="submit" class="btn btn-default" name="btnSignup">Sign Up</button>
-      &ensp;
+      &emsp;
       <button type="button" class="btn btn-default" name="btnCancel">Cancel</button>
     </div>
   </div>
