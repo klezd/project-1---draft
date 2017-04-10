d753319aace2d5012afadd70ea278e8409c0cd45
Duc Le <hieuduc194@gmail.com>
2017-04-10 10:14:51 +0300

    menu layout is ready

--- menu_book.php
+++ menu_book.php
@@ -1,3 +1,55 @@
 <?php include "navigator.php"; ?>
-
+<form>
+	  <label for="searchDate">You are looking for menu on </label>
+    <input type="date" name="searchDate" value="">
+	  <input type="submit" name="btnSend" value="Send">
+</form>
+<br>
+<p>Main Course</p>
+	<div class="centerText">
+		<table border="0">
+		  <tr>
+			<td><img></td>
+			<td><img></td>
+			<td><img></td>
+		  </tr>
+		  <tr>
+		    <td>Name 1</td>
+			<td>Name 2</td>
+			<td>Name 3</td>
+		  </tr>
+		  <tr>
+		    <td>Price 1</td>
+			<td>Price 2</td>
+			<td>Price 3</td>
+		  </tr>
+		  <tr>
+		    <td><button type="button">Add to cart</button></td>
+			<td><button type="button">Add to cart</button></td>
+			<td><button type="button">Add to cart</button></td>
+		  </tr>
+		</table>
+	</div>
+	<br>
+	<p>Dessert</p>
+	<div class="centerText">
+		<table border="0">
+		  <tr>
+			<td><img></td>
+			<td><img></td>
+		  </tr>
+		  <tr>
+		    <td>Name 1</td>
+			<td>Name 2</td>
+		  </tr>
+		  <tr>
+		    <td>Price 1</td>
+			<td>Price 2</td>
+		  </tr>
+		  <tr>
+		    <td><button type="button">Add to cart</button></td>
+			<td><button type="button">Add to cart</button></td>
+		  </tr>
+		</table>
+  </div>  
 <?php include "footer.php"; ?>
