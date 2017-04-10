e0769fc2bed35720657f7d3e31690d598d76758b
Duc Le <hieuduc194@gmail.com>
2017-04-10 22:50:10 +0300

    bootstrap overriden successfully

--- navigator.php
+++ navigator.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title> Asian Aroma </title>
-        <link href="mycss.css" type="text/css"  rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="mycss.css" type="text/css"  rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="signupform.js"></script>
     </head>
