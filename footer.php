6cd82da47d9b4a13b47b6116e6ac42570f9976e8
Unknown <roxydao97@gmail.com>
2017-04-21 12:14:27 +0300

    change in footer width

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
     </div>
 <!-- footer -->
     <br>
-    <hr style="width: 65%;"/>
+    <hr style="width: 63%;"/>
     <div id="footer" class="content footerBackground">
           <div class="container centerText ">
             <div class="row">
-              <div class="col-sm-3">
+              <div class="col-sm-4">
                   <p><strong>Feedback</strong></p>
                   <p><a href="feedback.php"><img src="icon/email.png" alt=""></a></p>
               </div>
-              <div class="col-sm-3 seperateLine">
+              <div class="col-sm-4 seperateLine">
                 <p><strong>Find More</strong></p>
                 <p>
-                   <a href="#"><img src="icon/facebook.png" alt=""></a><span style="padding-left:48px;"></span>
-                   <a href="#"><img src="icon/instagram.png" alt=""></a><span style="padding-left:48px;"></span>
+                   <a href="#"><img src="icon/facebook.png" alt=""></a><span style="padding-left:20%;"></span>
+                   <a href="#"><img src="icon/instagram.png" alt=""></a><span style="padding-left:20%;"></span>
                    <a href="#"><img src="icon/twitter.png" alt=""></a>
                 </p>
                 <p style="color: #bef8d3;">a</p>
               </div>
-              <div class="col-sm-3">
+              <div class="col-sm-4">
                 <p><strong>Contact Us</strong></p>
                 <p>Phone</p>
                 <p>Email</p>
--- navigator.php
+++ navigator.php
@@ -1,12 +1,10 @@
 
 <html>
     <head>
-<<<<<<< HEAD
-        <!--<meta name="viewport" content="width=device-width, initial-sdcale=1">-->
-=======
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-sdcale=1">
->>>>>>> master
+
         <title> Asian Aroma </title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
