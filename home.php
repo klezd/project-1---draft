2cfa0a8542e52b6ef19f08a299b3130825ecab82
Unknown <roxydao97@gmail.com>
2017-04-27 12:19:46 +0300

    slideshow edited (homepage)

--- home.php
+++ home.php
@@ -4,31 +4,25 @@
 <h1 style="text-align:center;"> ASIAN AROMA </h1>
 <!-- img slide show -->
 <div class="slideshow-container">
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="menu/almond tofu.jpg" style="height: 380px;width:100%">
+    <div class="text">Almond Tofu</div>
+  </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="menu/almond tofu.jpg" style="height: 380px;width:100%">
-  <div class="text">Almond Tofu</div>
-</div>
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="menu/avocado pudding.jpg" style="height: 380px;width:100%">
+    <div class="text">Avocado Pudding</div>
+  </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="menu/avocado pudding.jpg" style="height: 380px;width:100%">
-  <div class="text">Avocado Pudding</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="menu/black sesame sweet soup.jpg" style="height: 380px;width:100%">
-  <div class="text">Black Sesame Sweet Soup</div>
-</div>
-
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="menu/black sesame sweet soup.jpg" style="height: 380px;width:100%">
+    <div class="text">Black Sesame Sweet Soup</div>
+  </div>
 </div>
 <br>
-
 <div style="text-align:center">
   <span class="dot" onclick="currentSlide(1)"></span>
   <span class="dot" onclick="currentSlide(2)"></span>
@@ -36,31 +30,24 @@
 </div>
 
 <script>
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 1800); // Change image every 2 seconds
 }
 </script>
 <?php include "footer.php"; ?>
